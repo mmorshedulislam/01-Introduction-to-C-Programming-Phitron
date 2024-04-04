d1e52567a602a68e5d9eb6d0e6f76d818d62de0c
Morshed-Office <morshed952640@gmail.com>
2024-04-05 00:48:47 +0600

    Alhamdulillah First assignment successfully done

---
+++ 04-Module-Assingment 01/1.print_it.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    printf("Hello, world! I am learning C programming language. ^_^\n"
+        "Programming is fun and challenging. /\\/\\/\\\n"
+        "I want to give my 100%% dedication to learn!\twill succeed one day.");
+    return 0;
+}
---
+++ 04-Module-Assingment 01/2.multiply.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    int A, B; 
+    scanf("%d %d", &A, &B); 
+    long long int mult = (long long int)A * B;
+    printf("%lld", mult);
+    return 0;
+}
